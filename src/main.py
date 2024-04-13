03eca9101a8d17baf91f246b4603b5e8703312d7
Aniketh <aaatipamula@gmail.com>
2024-04-13 16:24:01 -0500

    move file location and tie in with flask

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 app = Flask("hackku24")
 
 @app.get("/")
 def index():
-    return "Hello world!", 200
+    return send_from_directory("static", "/page/index.html")
+
+@app.get("/ks/kansas-city")
+def ks_page():
+    return send_from_directory("static/page", "ks-kansas-city.html")
